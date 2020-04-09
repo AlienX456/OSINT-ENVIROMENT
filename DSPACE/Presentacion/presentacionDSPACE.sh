b838fddc9066034f97333ce6c6111acd491ed6d1
alienx456 <estebanelias27@gmail.com>
2020-04-09 12:23:50 -0500

    Corregido presentación

--- DSPACE/Presentacion/presentacionDSPACE.sh
+++ DSPACE/Presentacion/presentacionDSPACE.sh
@@ -1,3 +1,3 @@
-cp /html/* /opt/dspace/config/ &&\
-cp /img/logo_distri.png /opt/tomcat/webapps/jspui/image/dspace-logo-only.png &&\
-cp /img/logo_distri.gif /opt/tomcat/webapps/jspui/image/logo.gif &&\
+cp /html/* /opt/dspace/config/
+cp /img/logo_distri.png /opt/tomcat/webapps/jspui/image/dspace-logo-only.png
+cp /img/logo_distri.gif /opt/tomcat/webapps/jspui/image/logo.gif
